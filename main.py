600070926985e6b2cb55dd5480510ce5e567841e
Yanil <yanilkumawat856@gmail.com>
2026-03-23 19:09:30 +0530

    data_ingestion_work_completed

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from src.cnnClassifier import logger
+# from src.cnnClassifier import logger
 
-print(logger.info("Welcome to my custom log"))
+# print(logger.info("Welcome to my custom log"))
+
+#from src.cnnClassifier import logger
+# from src.cnnClassifier import logger
+
+# print(logger.info("Welcome to my custom log"))
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
