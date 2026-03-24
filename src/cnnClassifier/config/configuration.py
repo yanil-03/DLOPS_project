1f7bc354abc80e94b1c3e59ad423c74de6f46d8e
Yanil <yanilkumawat856@gmail.com>
2026-03-24 12:02:17 +0530

    prepare_base_model_pipeline_connected

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
 from src.cnnClassifier.entity.config_entity import (
-    DataIngestionConfig
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+
     
 )
 from pathlib import Path
@@ -28,5 +30,21 @@ class ConfigurationManager:
             source_URL=config['source_URL'],
             local_data_file=Path(config['local_data_file']),
             unzip_dir=Path(config['unzip_dir'])
+        )
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config['prepare_base_model']
+        params = self.params
+        create_directories([config['root_dir']])
+
+        return PrepareBaseModelConfig(
+            root_dir=Path(config['root_dir']),
+            base_model_path=Path(config['base_model_path']),
+            updated_base_model_path=Path(config['updated_base_model_path']),
+            params_image_size=params['IMAGE_SIZE'],
+            params_learning_rate=params['LEARNING_RATE'],
+            params_include_top=params['INCLUDE_TOP'],
+            params_weights=params['WEIGHTS'],
+            params_classes=params['CLASSES']
         )
     
